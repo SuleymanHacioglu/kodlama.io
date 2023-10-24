37c645871180fee2d933f29de289373c0bbbc602
Süleyman Hacıoğlu <suleyman0798@gmail.com>
2023-10-24 16:32:34 +0300

    Ödev2

---
+++ miniProjeSesliHarfler/src/miniProjeSesliHarfler/miniProjeSesliHarfler.java
@@ -0,0 +1,20 @@
+package miniProjeSesliHarfler;
+
+public class miniProjeSesliHarfler {
+
+	public static void main(String[] args) {
+		char harf ='e';
+		switch(harf) {
+		case 'a':
+		case 'ı':
+		case 'o':
+		case 'u':
+		System.out.println("Kalın sesli harf.");
+		break;
+		default:
+		System.out.println("İnce sesli harf.");
+		}
+
+	}
+
+}
