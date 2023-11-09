9e149e02be37ff1ab19379eed4f4096700cb9f8b
Süleyman Hacıoğlu <suleyman0798@gmail.com>
2023-11-09 17:23:25 +0300

    Video3Ödev

---
+++ abstractClasses/src/abstractClasses/Main.java
@@ -0,0 +1,13 @@
+package abstractClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		WomanGameCalculator womanGamerCalculator = new WomanGameCalculator();
+		womanGamerCalculator.hesapla();
+		womanGamerCalculator.gameOver();
+		GameCalculator gameCalculator = new WomanGameCalculator();
+
+	}
+
+}
